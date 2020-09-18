236505da2aee9d4749adfec980ade14b8dfd0c68
2298814528 <2298814528@qq.com>
2020-09-18 16:01:38 +0800

    朱建军  代码生成器包错误  2020/9/18

--- src/test/java/com/group9/apply/ApplyApplicationTests.java
+++ src/test/java/com/group9/apply/ApplyApplicationTests.java
@@ -1,7 +1,11 @@
 package com.group9.apply;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.group9.apply.entity.User;
+import com.group9.apply.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplyApplicationTests {
 
+    @Autowired
+    UserService userService;
+
     @Test
     public void contextLoads() {
+        System.out.println(userService);
+        User one = userService.getOne(new QueryWrapper<User>().eq("username", "张三"));
+        System.out.println(one);
     }
 
 }
