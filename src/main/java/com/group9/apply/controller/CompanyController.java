a45821ceba18fb85b9f39870e6edaf003799f6d9
My <1409298549@qq.com>
2020-09-20 16:02:00 +0800

    增加result实体用于前端接收数据、引入fastjson、注册、登陆后台实现  张发兴  2020/9/20

--- src/main/java/com/group9/apply/controller/CompanyController.java
+++ src/main/java/com/group9/apply/controller/CompanyController.java
@@ -1,8 +1,13 @@
 package com.group9.apply.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.group9.apply.entity.Company;
+import com.group9.apply.entity.Result;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.group9.apply.controller.BaseController;
 
@@ -18,4 +23,21 @@ import com.group9.apply.controller.BaseController;
 @RequestMapping("/company")
 public class CompanyController extends BaseController {
 
+    /**
+     * 添加企业信息
+     */
+    @PostMapping("/addCompany")
+    @ResponseBody
+    public Result addDate(String name, String address, String description){
+        Company company = new Company();
+        company.setName(name);
+        company.setAddress(address);
+        company.setDescption(description);
+        companyService.save(company);
+        return new Result(200,"add success",null);
+    }
+
+//    public Result upDate(){
+//
+//    }
 }
